fd3a7ae217c3b649d48b3b6817840bea0c7f4acb
JacobBartlett24 <jacobart@ttu.edu>
2022-09-11 12:49:57 -0500

    Navbar functionality

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import styled from "styled-components";
+import "./App.css";
 
-class App extends Component{
-  
-  render(){
-    return(
-      <div>  
-        <Header title = 'Record Store'/>
-        <Main />
-        <Footer title = 'Footer'/>
-      </div>
-    );
-  }
+function App(){
+
+  return(
+    <div>  
+      <Header title = 'Record Store'/>
+      <Main />
+      <Footer title = 'Footer'/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
+import "./Header.css";
 
 const HeaderStyle = styled.div`
   background-color: black;
   color: white;
   margin: 0;
-  padding: 20px;
+  height: 50px;
+  padding: 1rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -20,51 +23,41 @@ const HeaderStyle = styled.div`
   }
 `
 
-var NavbarStyle = styled.div`
-  display: none;
+const NavbarStyle = styled.div`
+  
+  display: inline-block;
+  background-color: gray;
 `
 
 
 
-class Header extends Component{
+function Header(props){
 
-  constructor(props){
+  const [navbar, navbarEnabled] = useState(false);
 
-    super(props)
+  const toggler = () =>{
 
-    this.state = {
-
-    }
+    navbarEnabled((prev) => !prev);
+    
   }
 
-  enableSidebar = (e) =>{
-    NavbarStyle = `
-      display: show;
-    `
-  
-    console.log('clicked');
-  }
-
-  render(){
-    const {title} = this.props;
   return(
-    <div>
-      
+    <div style={{height: "inherit"}}>
       <HeaderStyle>
-        <a href="#ss" onClick={this.enableSidebar}><FontAwesomeIcon icon={faBars} /></a>
-        <h1>{title}</h1>
-        <a href = "#ss">Menu</a>
+        <a href="#ss" onClick={toggler}><FontAwesomeIcon icon={faBars} /></a>
+        <h1>{props.title}</h1>
+        <a href = "#ss"><FontAwesomeIcon icon={faCartShopping} /></a>
       </HeaderStyle>
-      <NavbarStyle>
+      <NavbarStyle className={navbar ? "" : "active"}>
         <ul>
-          <li><FontAwesomeIcon icon={faRecordVinyl} />Store</li>
-          <li>About Us</li>
-          <li>FAQ</li>
-          <li>Shipping Info</li>
+          <li><FontAwesomeIcon icon={faRecordVinyl} /> Store</li>
+          <li><FontAwesomeIcon icon={faRecordVinyl} /> About Us</li>
+          <li><FontAwesomeIcon icon={faRecordVinyl} /> FAQ</li>
+          <li><FontAwesomeIcon icon={faRecordVinyl} /> Shipping Info</li>
         </ul>
       </NavbarStyle>
     </div>
-  );}
+  );
 }
 
 export default Header;
