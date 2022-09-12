73e6c4ca07626521c9281853c904c91fceda3992
JacobBartlett24 <jacobart@ttu.edu>
2022-09-11 20:54:14 -0500

    Import routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 import "./Header.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Store from "./Store"
 
 const HeaderStyle = styled.div`
   background-color: black;
@@ -50,11 +57,18 @@ function Header(props){
       </HeaderStyle>
       <NavbarStyle className={navbar ? "" : "active"}>
         <ul>
-          <li><FontAwesomeIcon icon={faRecordVinyl} /> Store</li>
+          <li>
+          
+            <FontAwesomeIcon icon={faRecordVinyl} />
+            Store
+          
+          </li>
           <li><FontAwesomeIcon icon={faRecordVinyl} /> About Us</li>
           <li><FontAwesomeIcon icon={faRecordVinyl} /> FAQ</li>
           <li><FontAwesomeIcon icon={faRecordVinyl} /> Shipping Info</li>
         </ul>
+        s
+        
       </NavbarStyle>
     </div>
   );
---
+++ src/components/Store.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import Footer from "./Footer"
+
+function Store(props){
+
+
+  return(
+    
+    <div>
+      <Header />
+      <Footer />
+    </div>
+    
+  );
+  
+}
+
+export default Store;
