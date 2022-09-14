05cc6013b0a04e7e45d32ece2ec08a18c6db3586
JacobBartlett24 <jacobart@ttu.edu>
2022-09-14 13:35:30 -0500

    Admin form styling

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -5,15 +5,43 @@ import logo from "../images/sample-stamp-rubber-style-red-260nw-1811246308.png";
 const AdminMenuStyle= styled.div`
 height: 100vh;
 width: 100vw;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+const FormStyle = styled.div`
+display: flex;
+flex-direction: column;
+height: 50%;
+width: 50%;
+align-items: center;
+justify-content: center;
+background-color: #f2f2f2;
+border-radius: 3px;
+input{
+  margin: 1rem 0 1rem 0;
+  display: block;
+}
 `
 
 function Admin(props){
   return(
     <AdminMenuStyle>
-      <div>
+      <FormStyle>
         <h1>Admin Menu</h1>
+        <form>
+          <label>
+            Artist Name:
+          </label>
+          <input></input>
+          <label>
+            Price:
+          </label>
+          <input></input>
+        </form>
         <button>Add Vinyl</button>
-      </div>
+      </FormStyle>
     </AdminMenuStyle>
   )
 }
